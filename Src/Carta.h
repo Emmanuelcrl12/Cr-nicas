a0211b94bcb6ddfd4b7d16ef7b594af963d83cfd
Emmanuelcrl12 <emmanuelrendon7848@gmail.com>
2025-03-23 12:46:48 -0500

    Update Carta.h

--- Src/Carta.h
+++ Src/Carta.h
@@ -13,26 +13,32 @@ class Carta{
         string paleta[4]= {BG_RED, BG_GREEN, BG_BLUE, BG_YELLOW};
         int numero;//poder de la carta
         int color;
+        bool estado;
         Carta();
-        Carta(int color, int numero);
+        Carta(int color, int numero, bool estado);
         void mostrar();
 };
 
 Carta::Carta(){
-    
+
+    estado=true;
     color = 0;
     numero = 0;
     
 }
 
-Carta::Carta (int color, int numero){
+Carta::Carta (int color, int numero, bool estado){
     this->color = color;
     this->numero = numero;
+    this->estado=estado;
 }
 
-
 void Carta::mostrar(){
-    cout << paleta[color] << " " << numero << " "<< RESET; 
+    if(estado){
+      cout << "[X]";  // Mostramos la carta bocabajo
+    }else{
+    cout << paleta[color] << " " << numero << " "<< " \x1B[0m ";     
+    }
 }
 
 #endif
