a030e36453e00e6ae5381ae7f66ef0afe6a8d1ac
Samuel-ITM <samuelsalazar1130049@correo.itm.edu.co>
2025-03-22 23:59:32 -0500

    impementacion tablero

---
+++ Src/Tablero.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "./Jugador.h"
+
+
+
+class Tablero{
+
+    public:
+        vector<Jugador> jugadores;
+        vector<Carta> cartasTablero;
+        Tablero();
+        void jugarRonda();
+        void mostrarTablero();
+
+
+};
+
+
+Tablero::Tablero(){
+    
+}
+
+void Tablero::jugarRonda(){
+    
+}
+
+void Tablero::mostrarTablero(){
+    
+}
