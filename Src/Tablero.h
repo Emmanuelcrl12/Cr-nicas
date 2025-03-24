246fb8ed18ac76e0bd04d341d9da082ad48f0172
Samuel-ITM <samuelsalazar1130049@correo.itm.edu.co>
2025-03-23 22:43:29 -0500

    avances menores

--- Src/Tablero.h
+++ Src/Tablero.h
@@ -16,6 +16,7 @@ class Tablero{
         
         vector<Jugador> jugadores;
         vector<Carta> cartasTablero;
+        Tablero(Carta& cartaSeleccionada);
         Tablero();
         void jugarRonda();
         void mostrarTablero();
@@ -23,16 +24,20 @@ class Tablero{
 
 };
 
-
 Tablero::Tablero(){
     
+}
+
+Tablero::Tablero(Carta& cartaSeleccionada){
+    cartasTablero.push_back(cartaSeleccionada);
 }
 
 void Tablero::mostrarTablero(){
     cout << "-----CARTAS EN JUEGO-------\n ";
     for (int i = 0; i < cartasTablero.size(); i++){
         cartasTablero[i].mostrar();
-        cout << " ";
+        cout << "existo ";
+        
     }
     cout << "\n";
 }
