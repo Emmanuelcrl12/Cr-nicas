37e4e06c056a2b06dcdf4e750f7a1dc449a142e4
Emmanuelcrl12 <emmanuelrendon7848@gmail.com>
2025-04-16 22:01:42 -0500

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -9,42 +9,41 @@ int main(){
     srand(time(0));
     Mazo mazo1;
     mazo1.mostrar();
-    system("cls");
 
-    
     mazo1.barajar();
     cout<<" "<<endl;
-    //mazo1.mostrar();
-    system("pause");
+    mazo1.mostrar();
+
     Mano mano;
     Tablero tablero;
-
     
     int jugadores;
     cout << "\nIngrese el n�mero de jugadores: ";
     cout << "Recuerde ingresar un valor par o 3, o 9\n ";
     cin >> jugadores;
-    
+    bool gano=true;
     
 
     vector<Mano> manos = mazo1.repartir(jugadores);
-    while(true){
-    
-        tablero.jugarRonda(manos, tablero, jugadores);
-
-        int ganador = tablero.compararGanador();
-        tablero.mandarGanador(manos, ganador);
-
-        for (int i = 0; i < jugadores; i++) {
-            if (manos[i].cartasM.size() == 36) { 
-                system("cls");
-                cout << "¡El jugador " << i + 1 << " ha ganado el juego con todas las cartas!" << endl;
-                system("pause");
-                return 0;
+    while(gano){
+    tablero.jugarRonda(manos, tablero, jugadores);
+    int ganador = tablero.compararGanador();
+    tablero.vaciar(ganador,manos);
+         int jugadoresConCartas = 0;
+         int ultimoJugadorConCartas = -1; 
+            for (int i = 0; i < jugadores; i++) {
+                if (manos[i].cartasM.size() > 0) {
+                  jugadoresConCartas++;
+                  ultimoJugadorConCartas = i; 
+                  jugadoresConCartas++;
+                }
+             }
+            if (jugadoresConCartas == 1) {
+              cout << "¡El jugador " << ultimoJugadorConCartas + 1 << " ha ganado el juego!" << endl;
+              gano=false;
+              break;
             }
-        }
 
-
-    }
+       }      
     return 0;
 }
